9ae8a0155c9b856c9178d70d975dce433d581fd2
guindosaros <47152090+guindosaros@users.noreply.github.com>
2022-02-13 02:03:48 +0000

    update code

--- pharmaapi/production.py
+++ pharmaapi/production.py
@@ -1,17 +1,11 @@
+import django_heroku
 from .base import *
+
 
 DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ["*"]
-
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+ALLOWED_HOSTS = ["medoc-api.herokuapp.com"]
 
 
 STATIC_URL = "/static/"
@@ -21,3 +15,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Media files config
 MEDIA_URL = "/media/"  # or any prefix you choose
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+django_heroku.settings(locals())
