8ef632fd56872044a78f9871e5f89f8ac9996b3d
guindosaros <47152090+guindosaros@users.noreply.github.com>
2022-02-22 03:59:06 +0000

    Update production.py

--- pharmaapi/production.py
+++ pharmaapi/production.py
@@ -20,7 +20,6 @@ ALLOWED_HOSTS = ["*", "medoc-api.herokuapp.com"]
 
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
